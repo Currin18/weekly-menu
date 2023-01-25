be00a44b277ae6eb10e41f483839692742e97b11
Jesús Moreira <jesus.moreira@indigitall.com>
2023-01-25 09:39:07 +0100

    Update Calendar date based

--- app/src/main/java/com/jesusmoreira/weeklymenu/presentation/components/Calendar.kt
+++ app/src/main/java/com/jesusmoreira/weeklymenu/presentation/components/Calendar.kt
@@ -1,26 +1,32 @@
 package com.jesusmoreira.weeklymenu.presentation.components
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import java.time.LocalDate
+import java.util.Date
 
 val MONTH_DAYS = listOf("L", "M", "X", "J", "V", "S", "D")
 
 @Composable
-fun Calendar() {
+fun Calendar(date: LocalDate) {
     Column {
         WeekRow(MONTH_DAYS)
+        MonthGrid(date)
     }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun CalendarPreview() {
-    Calendar()
+    Calendar(LocalDate.now())
 }
 
 @Composable
@@ -29,10 +35,15 @@ fun WeekRow(listItems: List<String>) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceAround
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         listItems.forEach {
-            Text(text = it)
+            Text(
+                text = it,
+                modifier = Modifier.width(24.dp),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -44,17 +55,28 @@ fun WeekRowPreview() {
 }
 
 @Composable
-fun MonthGrid() {
-    val calendar = java.util.Calendar.getInstance()
-    val day = calendar.get(java.util.Calendar.DAY_OF_MONTH)
-    val month = calendar.get(java.util.Calendar.MONTH)
-    val year = calendar.get(java.util.Calendar.YEAR)
+fun MonthGrid(date: LocalDate) {
+    val month = LocalDate.of(date.year, date.month, 1)
+    val dayOfWeek = month.dayOfWeek.value - 1
+    val lengthOfMonth = month.lengthOfMonth() - 1
+    val weeksList = mutableListOf<MutableList<String>>()
+    for (i in 0.. lengthOfMonth + dayOfWeek) {
+        if (i % 7 == 0) weeksList.add(mutableListOf("", "", "", "", "", "", ""))
+        if (i >= dayOfWeek) weeksList.last()[i % 7] = "${i - dayOfWeek + 1}"
+    }
 
-    Text(text = "day: $day, month: $month, year: $year")
+    Column(
+    ) {
+        if (weeksList.isNotEmpty()) {
+            for (week in weeksList) {
+                WeekRow(listItems = week)
+            }
+        }
+    }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun MonthGridPreview() {
-    MonthGrid()
+    MonthGrid(LocalDate.now())
 }
